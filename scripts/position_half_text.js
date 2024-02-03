12326dfa83b557d45968210aaee66ddf8d6a61b2
Serban <iustinianserban@gmail.com>
2024-02-03 22:22:06 +0200

    Create index page Add Navbar, Banner, Last-Edition, JobShop, HTS and Footer sections with seperate style files

---
+++ scripts/position_half_text.js
@@ -0,0 +1,9 @@
+window.onload = function(){
+    var half_text = document.getElementById("half-text");
+    half_text.style.marginTop = -half_text.offsetHeight / 2 + "px";
+}
+
+window.onresize = function(){
+    var half_text = document.getElementById("half-text");
+    half_text.style.marginTop = -half_text.offsetHeight / 2 + "px";
+}
