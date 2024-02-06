c6d554e1a7157f58419e741105e58db45c2ea767
Serban <iustinianserban@gmail.com>
2024-02-06 21:16:03 +0200

    Implement script to play videos when ready

---
+++ scripts/play_videos.js
@@ -0,0 +1,7 @@
+$( document ).ready(function() {
+    document.getElementById('banner-video').play();
+    let last_edition_video =  document.getElementById('last-edition-video');
+
+    if(last_edition_video != undefined) last_edition_video.play(); 
+   
+});
